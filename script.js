0ad3eed267b449280c3e32e141c85c63fc40edaa
Mr. Ryt <flaviosobbin04@gmail.com>
2023-06-25 17:05:27 +0000

    right track

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 function age() {
   const d = new Date();
-  var currentDate = d.getDate();
+  const currentDate = d.getDate();
   var currentMonth = d.getMonth() + 1;
   var currentYear = d.getFullYear();
   const lastDate = new Date(currentYear, currentMonth, 0).getDate();
 
-  const inputDate = parseInt(document.getElementById("inputDate").value);
-  const inputMonth = parseInt(document.getElementById("inputMonth").value);
-  const inputYear = parseInt(document.getElementById("inputYear").value);
+  const inputDate = document.getElementById("inputDate").value;
+  const inputMonth = document.getElementById("inputMonth").value;
+  const inputYear = document.getElementById("inputYear").value;
 
   const label = document.querySelectorAll("label");
   const input = document.querySelectorAll("input");
@@ -17,16 +17,59 @@ function age() {
   const resultMonth = document.getElementById("resultMonth");
   const resultYear = document.getElementById("resultYear");
 
-  if (input[0] === "") {
-    log[0].innerText = "This field is required";
+  let a = 1,
+    b = 2,
+    c = 0.1;
+  switch (inputDate) {
+    case "":
+      label[0].style.color = "var(--lightRed)";
+      input[0].style.borderColor = "var(--lightRed)";
+      log[0].innerText = "This field is required";
+      resultDate.innerText = "--";
+      break;
+    default:
+      a = 0;
+      label[0].style.color = "var(--smokeyGrey)";
+      input[0].style.borderColor = "var(--lightGrey)";
+      log[0].innerText = "";
+  }
+  switch (inputMonth) {
+    case null || "":
+      label[1].style.color = "var(--lightRed)";
+      input[1].style.borderColor = "var(--lightRed)";
+      log[1].innerText = "This field is required";
+      resultMonth.innerText = "--";
+      break;
+    default:
+      b = 0;
+      label[1].style.color = "var(--smokeyGrey)";
+      input[1].style.borderColor = "var(--lightGrey)";
+      log[1].innerText = "";
+  }
+  switch (inputYear) {
+    case null || "":
+      label[2].style.color = "var(--lightRed)";
+      input[2].style.borderColor = "var(--lightRed)";
+      log[2].innerText = "This field is required";
+      resultYear.innerText = "--";
+      break;
+    default:
+      c = 0;
+      label[2].style.color = "var(--smokeyGrey)";
+      input[2].style.borderColor = "var(--lightGrey)";
+      log[2].innerText = "";
+  }
+
+  let check = a === b && a === c && b === c;
+  if (!check) {
   } else {
-    let calcDate = currentDate - inputDate;
-    if (inputDate > lastDate) {
+    let calcDate = currentDate - parseInt(inputDate);
+    if (inputDate < 0 || inputDate > lastDate) {
       label[0].style.color = "var(--lightRed)";
       input[0].style.borderColor = "var(--lightRed)";
       log[0].innerText = "Must be a valid date";
     } else if (calcDate < 0) {
-      calcDate = calcDate + lastDate;
+      calcDate += lastDate;
       currentMonth--;
       resultDate.innerText = calcDate;
       label[0].style.color = "var(--smokeyGrey)";
@@ -39,10 +82,10 @@ function age() {
       log[0].innerText = "";
     }
 
-    let calcMonth = currentMonth - inputMonth;
-    if (inputMonth > 12) {
+    let calcMonth = currentMonth - parseInt(inputMonth);
+    if (inputMonth < 0 || inputMonth > 12) {
       label[1].style.color = "var(--lightRed)";
-      input[1].style.borderColor = "var(--lightGrey)";
+      input[1].style.borderColor = "var(--lightRed)";
       log[1].innerText = "Must be a valid month";
     } else if (calcMonth < 0) {
       calcMonth += 12;
@@ -58,8 +101,8 @@ function age() {
       log[1].innerText = "";
     }
 
-    let calcYear = currentYear - inputYear;
-    if (calcYear < 0) {
+    let calcYear = currentYear - parseInt(inputYear);
+    if (inputYear < 0 || calcYear < 0) {
       label[2].style.color = "var(--lightRed)";
       input[2].style.borderColor = "var(--lightRed)";
       log[2].innerText = "Must be in the past";
