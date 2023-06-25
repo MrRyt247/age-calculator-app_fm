9d370acebe415ab4e2937925741bd16aa545e331
Mr. Ryt <flaviosobbin04@gmail.com>
2023-06-25 15:10:43 +0000

    a step in the right direction

--- script.js
+++ script.js
@@ -1,41 +1,73 @@
+function age() {
+  const d = new Date();
+  var currentDate = d.getDate();
+  var currentMonth = d.getMonth() + 1;
+  var currentYear = d.getFullYear();
+  const lastDate = new Date(currentYear, currentMonth, 0).getDate();
 
-    const d = new Date();
-    var currentDay = d.getDay();
-    var currentMonth = d.getMonth() + 1;
-    var currentYear = d.getFullYear();
+  const inputDate = parseInt(document.getElementById("inputDate").value);
+  const inputMonth = parseInt(document.getElementById("inputMonth").value);
+  const inputYear = parseInt(document.getElementById("inputYear").value);
 
-    const inputDay = document.getElementById("inputDay").value;
-    const inputMonth = document.getElementById("inputMonth").value;
-    const inputYear = document.getElementById("inputYear").value;
-    const month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+  const label = document.querySelectorAll("label");
+  const input = document.querySelectorAll("input");
+  const log = document.getElementsByClassName("log");
 
-    const resultDay = document.getElementById("resultDay");
-    const resultMonth = document.getElementById("resultMonth");
-    const resultYear = document.getElementById("resultYear");
+  const resultDate = document.getElementById("resultDate");
+  const resultMonth = document.getElementById("resultMonth");
+  const resultYear = document.getElementById("resultYear");
 
-    inputDay.oninput = () => {
-        resultDay.innerText = inputDay;
-    };
+  if (input[0] === "") {
+    log[0].innerText = "This field is required";
+  } else {
+    let calcDate = currentDate - inputDate;
+    if (inputDate > lastDate) {
+      label[0].style.color = "var(--lightRed)";
+      input[0].style.borderColor = "var(--lightRed)";
+      log[0].innerText = "Must be a valid date";
+    } else if (calcDate < 0) {
+      calcDate = calcDate + lastDate;
+      currentMonth--;
+      resultDate.innerText = calcDate;
+      label[0].style.color = "var(--smokeyGrey)";
+      input[0].style.borderColor = "var(--lightGrey)";
+      log[0].innerText = "";
+    } else {
+      resultDate.innerText = calcDate;
+      label[0].style.color = "var(--smokeyGrey)";
+      input[0].style.borderColor = "var(--lightGrey)";
+      log[0].innerText = "";
+    }
 
-    let supYear = 0;
+    let calcMonth = currentMonth - inputMonth;
+    if (inputMonth > 12) {
+      label[1].style.color = "var(--lightRed)";
+      input[1].style.borderColor = "var(--lightGrey)";
+      log[1].innerText = "Must be a valid month";
+    } else if (calcMonth < 0) {
+      calcMonth += 12;
+      currentYear--;
+      resultMonth.innerText = calcMonth;
+      label[1].style.color = "var(--smokeyGrey)";
+      input[1].style.borderColor = "var(--lightGrey)";
+      log[1].innerText = "";
+    } else {
+      resultMonth.innerText = calcMonth;
+      label[1].style.color = "var(--smokeyGrey)";
+      input[1].style.borderColor = "var(--lightGrey)";
+      log[1].innerText = "";
+    }
 
-    inputMonth.oninput = () => {
-        let result = currentMonth - inputMonth;
-        let supYear1 = 0;
-        if(inputMonth > 12) {
-            resultMonth.innerText = "error";
-        } else if (result < 0) {
-            result += 12;
-            supYear1 = 1;
-            windows.supYear = supYear1;
-            resultMonth.innerText = result;
-            } else {
-                supYear = 0;
-                resultMonth.innerText = result;
-        }
-    };
-
-    inputYear.oninput = () => { 
-        let result = currenntYear - inputYear - supYear;
-        result < 0 ? resultYear.innerText = "error": resultYear.innerText = result;
-    };
+    let calcYear = currentYear - inputYear;
+    if (calcYear < 0) {
+      label[2].style.color = "var(--lightRed)";
+      input[2].style.borderColor = "var(--lightRed)";
+      log[2].innerText = "Must be in the past";
+    } else {
+      resultYear.innerText = calcYear;
+      label[2].style.color = "var(--smokeyGrey)";
+      input[2].style.borderColor = "var(--lightGrey)";
+      log[2].innerText = "";
+    }
+  }
+}
